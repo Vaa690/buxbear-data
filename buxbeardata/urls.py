70b39bf195f0d767da43b9e00a1fa76e6018706f
vaa690 <v.abecassis@yahoo.com>
2022-09-12 22:55:08 -0700

    aded

--- buxbeardata/urls.py
+++ buxbeardata/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+from django.contrib import admin
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -9,4 +11,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("admin/", admin.site.urls),
+]
 ]

dc3e4c7c231f590b02e2cd762dd495304aec5d7f
vaa690 <v.abecassis@yahoo.com>
2022-09-12 22:59:28 -0700

    aded

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from api.models import User
+# Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+    )
+
+admin.site.register(User, UserAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+    email = models.TextField(db_index = True)
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    is_staff = models.BooleanField(blank=True, null=True, default=False)

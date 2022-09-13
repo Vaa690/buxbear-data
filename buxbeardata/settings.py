2bf95975561da45317cfe07f3e473fe9f2b8f1d0
vaa690 <v.abecassis@yahoo.com>
2022-09-12 22:43:24 -0700

    make it better

--- buxbeardata/settings.py
+++ buxbeardata/settings.py
@@ -129,3 +129,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
